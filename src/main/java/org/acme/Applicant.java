55f214c755383b923bea49916aa4b316a7f71758
Benjamin Wilson <whatsup@btwicode.com>
2021-03-24 13:15:24 -0700

    project now fully working restfully

--- src/main/java/org/acme/Applicant.java
+++ src/main/java/org/acme/Applicant.java
@@ -2,7 +2,39 @@ package org.acme;
 
 public class Applicant {
 
-  public String Name;
-  public Integer CreditScore;
-  public Boolean IsApproved;
+  private String name;
+  private int creditScore;
+  private boolean approved;
+
+  public Applicant() {}
+
+  public Applicant(String name, int creditScore) {
+    super();
+    this.name = name;
+    this.creditScore = creditScore;
+  }
+
+  public int getCreditScore() {
+    return this.creditScore;
+  }
+
+  public boolean isApproved() {
+    return this.approved;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setCreditScore(int creditScore) {
+    this.creditScore = creditScore;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setApproved(boolean approved) {
+    this.approved = approved;
+  }
 }
